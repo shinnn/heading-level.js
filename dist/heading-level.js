69ebbb739b21e7f906a781dad92b74f3998ecdae
shinnn <snnskwtnb@gmail.com>
2014-06-21 20:31:02 +0900

    update and run .editorconfig

--- dist/heading-level.js
+++ dist/heading-level.js
@@ -19,7 +19,7 @@ function headingLevel(tagName) {
   if (typeof tagName !== 'string') {
     throw new Error((("" + tagName) + " is not a string of tag name."));
   }
-  
+
   if (/^h\d/mi.test(tagName)) {
     var level = 1 * tagName.charAt(1);
     if (1 <= level && level <= 6) {
