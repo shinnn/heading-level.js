b08d4d6aefef3e8304ca215cd69a5abc990404a9
shinnn <snnskwtnb@gmail.com>
2014-09-06 07:25:17 +0900

    run tap-* binary explicitly using Node

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('build', ['lint', 'clean'], function() {
 });
 
 gulp.task('test', ['build'], function(cb) {
-  var cmd = 'node test.js | node_modules/.bin/tap-' + testReporter;
+  var cmd = 'node test.js | node ./node_modules/.bin/tap-' + testReporter;
 
   exec(cmd, function(err, stdout, stderr) {
     console.log(stdout);
